05a681cb0bb876fb30f6fbe6b7bf27199585fa85
小志小 <WuZhiXiang>
2020-02-25 16:45:06 +0800

    two commit

---
+++ sqlite/src/main/java/test/sqlite/demo/dao/UserDao.java
@@ -0,0 +1,11 @@
+package test.sqlite.demo.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import test.sqlite.demo.entity.User;
+
+/**
+ * @Author ZhiXiang
+ * @create 2020/2/25 16:01
+ */
+public interface UserDao extends BaseMapper<User> {
+}
---
+++ sqlite/src/main/java/test/sqlite/demo/entity/User.java
@@ -0,0 +1,17 @@
+package test.sqlite.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @Author ZhiXiang
+ * @create 2020/2/25 15:56
+ */
+@Data
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private String pwd;
+}
