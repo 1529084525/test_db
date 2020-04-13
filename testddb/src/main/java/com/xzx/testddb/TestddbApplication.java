62a76addf6e454424944270d88192e918aebb0d6
小志小 <WuZhiXiang>
2020-04-13 11:51:46 +0800

    1

---
+++ testddb/src/main/java/com/xzx/testddb/TestddbApplication.java
@@ -0,0 +1,16 @@
+package com.xzx.testddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 15290
+ */
+@SpringBootApplication
+public class TestddbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestddbApplication.class, args);
+    }
+
+}
